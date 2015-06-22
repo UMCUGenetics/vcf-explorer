2a5c254d1603037835463e8771686dcc10e7dd5d
Robert Ernst <rfernst@gmail.com>
2015-06-22 11:25:20 +0200

    Remove old flask views -> will probably use angular routing

--- vcfexplorer/frontend/views.py
+++ vcfexplorer/frontend/views.py
@@ -10,16 +10,4 @@ from . import bp
 
 @bp.route('/')
 def hello_world():
-    return 'hello_world'
-
-@bp.route('runs/')
-def runs():
-    return render_template('runs.html')
-
-@bp.route('samples/')
-def samples():
-    return render_template('samples.html')
-
-@bp.route('variants/')
-def variants():
-    return render_template('variants.html')
+    return render_template('index.html')
