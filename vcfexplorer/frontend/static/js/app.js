6e9e3b5591b605d0989288f6caf0d0c323555fb4
Robert Ernst <rfernst@gmail.com>
2015-06-22 13:00:57 +0200

    Angular app framework

---
+++ vcfexplorer/frontend/static/js/app.js
@@ -0,0 +1,29 @@
+var app = angular.module('vcfExplorerApp', ['ngRoute']);
+
+app.config(function ($routeProvider, $locationProvider) {
+
+  $routeProvider
+    .when('/', {
+      //controller: 'homeController',
+      templateUrl: '/static/partials/home.html'
+    })
+    .when('/runs', {
+      //controller: 'homeController',
+      templateUrl: '/static/partials/runs.html'
+    })
+    .when('/samples', {
+      //controller: 'homeController',
+      templateUrl: '/static/partials/samples.html'
+    })
+    .when('/variants', {
+      //controller: 'homeController',
+      templateUrl: '/static/partials/variants.html'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+
+  // use the HTML5 History API
+  $locationProvider.html5Mode(true);
+
+});
