ca598309e413c9135e55b86c931d8106d62d5f07
Robert Ernst <rfernst@gmail.com>
2015-07-13 07:54:37 +0200

    Samples view & controller

--- vcfexplorer/frontend/static/js/controllers/samplesController.js
+++ vcfexplorer/frontend/static/js/controllers/samplesController.js
@@ -6,9 +6,21 @@ samplesController.$inject = ['samplesService'];
 
 function samplesController(samplesService) {
   var vm = this;
-  vm.data = []
 
-  activate();
+  var columnDefs = [
+    {headerName: "Name", field: "samples", cellRenderer: function(params) {
+      return '<a href="/#/samples/'+params.data.samples+'">'+params.data.samples+'</a>';
+    }},
+    {headerName: "VCF", field: "vcf_file"},
+    {headerName: "Date", field: "upload_date"}
+  ];
+
+  vm.gridOptions = {
+    columnDefs: columnDefs,
+    rowData: null,
+    angularCompileRows: false,
+    ready: function(){ activate(); }
+  };
 
   function activate() {
     return getSamples().then(function() {
@@ -19,8 +31,9 @@ function samplesController(samplesService) {
   function getSamples() {
     return samplesService.getSamples()
       .then(function(data) {
-        vm.data = data;
-        return vm.data;
+        vm.gridOptions.rowData = data;
+        vm.gridOptions.api.onNewRows();
+        vm.gridOptions.api.sizeColumnsToFit();
       });
   }
 };
