4ada42d8fab4fe0348b183d26cb9960f72f2cf43
Robert Ernst <rfernst@gmail.com>
2015-07-14 14:55:09 +0200

    Move some functions and show metadata

--- vcfexplorer/frontend/static/js/controllers/runController.js
+++ vcfexplorer/frontend/static/js/controllers/runController.js
@@ -19,6 +19,7 @@ function runController(runService, $routeParams) {
     {headerName: "Alt", field: "alt"},
   ];
 
+  // Setup grid
   vm.gridOptions = {
     columnDefs: columnDefs,
     rowData: null,
@@ -30,36 +31,30 @@ function runController(runService, $routeParams) {
     },
   };
 
+  // Get run (meta)data functions
   function activateRun(runName) {
     return getRun(runName).then(function() {
       console.log('Activated Run View');
     });
   }
 
-  function getVariant(params) {
-    for (index=0, len=params.data.samples.length; index < len; ++index){
-      if(params.data.samples[index].id == params.colDef.field){
-        return params.data.samples[index].GT
-      };
-    };
-  };
-
   function getRun(runName) {
     return runService.getRun(runName)
       .then(function(data) {
-        vm.run = data;
-        // Add samples to columnDefs
-        var index, len
-        for (index=0, len=vm.run.samples.length; index < len; ++index){
-          sampleColumnDef = { headerName: vm.run.samples[index], field:vm.run.samples[index], valueGetter: getVariant };
-          vm.gridOptions.columnDefs = vm.gridOptions.columnDefs.concat(sampleColumnDef);
-        };
-        vm.gridOptions.api.onNewCols();
-        vm.gridOptions.api.sizeColumnsToFit();
-        return vm.run;
-      });
+      vm.run = data;
+      // Add samples to columnDefs
+      var index, len
+      for (index=0, len=vm.run.samples.length; index < len; ++index){
+        sampleColumnDef = { headerName: vm.run.samples[index], field:vm.run.samples[index], valueGetter: getVariant };
+        vm.gridOptions.columnDefs = vm.gridOptions.columnDefs.concat(sampleColumnDef);
+      };
+      vm.gridOptions.api.onNewCols();
+      vm.gridOptions.api.sizeColumnsToFit();
+      return vm.run;
+    });
   }
 
+  // Get run variants functions
   function activateRunVariants(runName) {
     return getRunVariants(runName).then(function() {
       console.log('Activated RunVariants View');
@@ -68,10 +63,17 @@ function runController(runService, $routeParams) {
 
   function getRunVariants(runName) {
     return runService.getRunVariants(runName)
-      .then(function(data) {
-        vm.gridOptions.rowData = data;
-        vm.gridOptions.api.onNewRows();
-        vm.gridOptions.api.sizeColumnsToFit();
-      });
+    .then(function(data) {
+      vm.gridOptions.rowData = data;
+      vm.gridOptions.api.onNewRows();
+      vm.gridOptions.api.sizeColumnsToFit();
+    });
   }
+  // Valuegetter for sample genotype per variant.
+  function getVariant(params) {
+    for (index=0, len=params.data.samples.length; index < len; ++index){
+      if(params.data.samples[index].id == params.colDef.field){
+        return params.data.samples[index].GT
+    }};
+  };
 };
