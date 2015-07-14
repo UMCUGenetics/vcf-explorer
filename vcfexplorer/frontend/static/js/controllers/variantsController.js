e73232f44d6afef6a882616b157202098126c307
Robert Ernst <rfernst@gmail.com>
2015-07-14 15:43:39 +0200

    Add variants grid

--- vcfexplorer/frontend/static/js/controllers/variantsController.js
+++ vcfexplorer/frontend/static/js/controllers/variantsController.js
@@ -8,8 +8,28 @@ function variantsController(variantsService) {
   var vm = this;
   vm.data = []
 
-  activate();
+  // Define columns
+  var columnDefs = [
+    {headerName: "ID", field: "_id", filter: 'text'},
+    {headerName: "Chr", field: "chr"},
+    {headerName: "Pos", field: "pos", filter: 'number'},
+    {headerName: "Ref", field: "ref"},
+    {headerName: "Alt", field: "alt"},
+    {headerName: "Sample Count", field: "samples", valueGetter: 'data.samples.length', filter: 'number'},
+  ];
 
+  // Setup grid
+  vm.gridOptions = {
+    columnDefs: columnDefs,
+    rowData: null,
+    enableSorting: true,
+    enableFilter: true,
+    ready: function(){
+      activate();
+    },
+  };
+
+  // Get variants function
   function activate() {
     return getVariants().then(function() {
       console.log('Activated Variants View');
@@ -19,8 +39,9 @@ function variantsController(variantsService) {
   function getVariants() {
     return variantsService.getVariants()
       .then(function(data) {
-        vm.data = data;
-        return vm.data;
+        vm.gridOptions.rowData = data;
+        vm.gridOptions.api.onNewRows();
+        vm.gridOptions.api.sizeColumnsToFit();
       });
   }
 };
