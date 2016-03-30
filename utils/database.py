12e9c00a17cabf93122621253eee5880149fc372
Robert Ernst <rfernst@gmail.com>
2016-03-30 15:09:45 +0200

    Fix indexes

--- utils/database.py
+++ utils/database.py
@@ -15,13 +15,15 @@ def create_indexes():
     db.vcfs.create_index("name")
     db.vcfs.create_index("samples")
     db.vcfs.create_index( [ ("filename", pymongo.ASCENDING), ("fileformat", pymongo.ASCENDING), ("filedate", pymongo.ASCENDING) ], sparse=True )
-    db.vcfs.create_index("run")
+    db.vcfs.create_index("INFO")
+    db.vcfs.create_index("FORMAT")
+    db.vcfs.create_index("FILTER")
 
     # Variant collection indexes
     db.variants.drop_indexes()
-    db.variants.create_index("samples.id")
-    db.variants.create_index("samples.run")
-    db.variants.create_index([("samples.id", pymongo.ASCENDING),("samples.filter", pymongo.ASCENDING)], sparse=True)
+    db.variants.create_index("samples.sample")
+    db.variants.create_index("samples.vcf_id")
+    db.variants.create_index([("samples.sample", pymongo.ASCENDING),("samples.filter", pymongo.ASCENDING)], sparse=True)
     db.variants.create_index( [ ("chr", pymongo.ASCENDING), ("pos", pymongo.ASCENDING), ("variant_info.chr2", pymongo.ASCENDING), ("variant_info.end", pymongo.ASCENDING), ("variant_info.svtype",         pymongo.ASCENDING),("variant_info.ct", pymongo.ASCENDING) ], sparse=True )
 
 def resetdb():
