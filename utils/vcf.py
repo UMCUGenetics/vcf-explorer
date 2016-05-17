97c6f29a05e796c279a163edda98d024368722de
Shira <xShiraVerduin@gmail.com>
2016-05-17 10:45:38 +0200

    VCF info field parsing

--- utils/vcf.py
+++ utils/vcf.py
@@ -58,9 +58,9 @@ def upload_vcf(vcf_file, vcf_type):
                         for variant_i, variant in enumerate(variants):
                             variant_id = '{}-{}-{}-{}'.format(variant['chr'], variant['pos'], variant['ref'], variant['alt'])
                             bulk_variants.find({'_id':variant_id}).upsert().update({
-                                '$push': {'samples': {'$each': variants_samples[variant_i]}},
-                                '$set': variant,
-                                }
+                               '$push': {'samples': {'$each': variants_samples[variant_i]}},
+                               '$set': variant,
+                               }
                             )
                             variant_count += len(variants)
 
@@ -74,12 +74,11 @@ def upload_vcf(vcf_file, vcf_type):
                         )
                         variant_count += 1
 
-                    # Upload variants in bulk
-                    if variant_count == 10000:
+                    #Upload variants in bulk
+                    if variant_count >= 500:
                         bulk_variants.execute()
                         bulk_variants = db.variants.initialize_unordered_bulk_op()
                         variant_count = 0
-
         # insert remaining variants and insert vcf metadata to vcfs collection
         bulk_variants.execute()
         db.vcfs.insert_one(vcf_metadata)
@@ -99,29 +98,66 @@ def vcf_header(line):
         line_metadata[data[0]] = {data[1] : data[2]}
 
     elif line.startswith('##INFO') or line.startswith('##FORMAT'):
-        data = re.search('(?:##)(\w+)(?:=<ID=)(\w+)(?:,Number=)(.+)(?:,Type=)(\w+)(?:,Description=")(.+)(?:">)',line).groups()
-        line_metadata[data[0]] = {
-            data[1] : {
-                'number': data[2],
-                'type': data[3],
-                'description': data[4]
+        try:
+          data = re.search('(?:##)(\w+)(?:=<ID=)(\S+)(?:,Number=)(.+)(?:,Type=)(\w+)(?:,Description=")(.+)(?:">)',line).groups()
+          line_metadata[data[0]] = {
+              data[1].replace(".", "-") : {
+                      'number': data[2],
+                      'type': data[3],
+                      'description': data[4]
+                }
             }
-        }
+        except AttributeError:
+          print "Error: Cannot parse this INFO line correctly: \n" + line
+          print "\nProgram is now closed."
+          sys.exit()
 
     elif line.startswith('##contig'):
-        data = re.search('(?:##contig=<ID=)(\w+)(?:,length=)(.+)(?:>)', line).groups()
-        line_metadata['contig'] = {
-            data[0] : {
-                'length' : data[1]
+        try:
+          data = re.search('(?:##contig=<ID=)(\w+)(?:,length=)(.+)(?:>)', line).groups()
+          line_metadata['contig'] = {
+              data[0] : {
+              'length' : data[1]
+                }
             }
-        }
+        except AttributeError:
+          print "Error: Cannot parse this contig line correctly: \n" + line
+          print "\nProgram is now closed."
+          sys.exit()
 
     elif line.startswith('#CHROM'):
         line_metadata['samples'] = line.split()[9:]
 
+    elif line.startswith('##GATKCommandLine.'):
+        try:
+            # gatk_commandline = re.search('(?:##)([GATKCommandLine\S]+)', line).groups()
+            # gatk_id = re.search('(?:=<ID=)(.+)', line).groups()
+            # gatk_version = re.search('(?:,Version=)(.+)', line).groups()
+            # gatk_date = re.search('(?:,Date=)(.+)', line).groups()
+            # gatk_epoch = re.search('(?:,Epoch=)(.+)', line).groups()
+            # gatk_commandlineoptions = re.search('(?:,CommandLineOptions=)(.+)(?:">)', line).groups()
+            data = re.search('(?:##)([GATKCommandLine\S]+)(?:=<ID=)(.+)(?:,Version=)(.+)(?:,Date=)(.+)(?:,Epoch=)(.+)(?:,CommandLineOptions=)(.+)(?:">)',line).groups()
+            line_metadata["GATKCommandLine"] = {
+                data[1] : {
+                'version': data[2],
+                'date': data[3],
+                'epoch': data[4],
+                'commandlineoptions': data[5]
+                }
+            }
+        except AttributeError:
+            #print "Error: Cannot parse this GATKCommand line correctly: \n" + line
+            #print "\nProgram is now closed."
+            return line_metadata
+
     else:
-        data = re.search('(?:##)(\w+)(?:=)(.+)',line).groups()
-        line_metadata[data[0]] = data[1]
+        try:
+          data = re.search('(?:##)(\S+)(?:=)(.+)',line).groups()
+          line_metadata[data[0]] = data[1]
+        except AttributeError:
+          print "Error: Cannot parse this GATKCommand line correctly: \n" + line
+          print "\nProgram is now closed."
+          sys.exit()
 
     return line_metadata
 
@@ -140,9 +176,11 @@ def gatk_line(line, vcf_metadata):
         list: variants_samples, list of variant samples lists.
     """
     # Setup general variables
+
     fields = line.split('\t')
     #external_ids = fields[2].split(',') # Split per alternative allele?
     gt_format = fields[8].split(':')
+    info_field = fields[7].split(";")
     variants = []
     variants_samples = []
 
@@ -150,13 +188,54 @@ def gatk_line(line, vcf_metadata):
     # 1/2 variants are splitted into two variants with genotype: 1/-
     # https://github.com/samtools/hts-specs/issues/77
     alt_alleles = fields[4].split(',')
+    info_dict = {}
+    """
+    Parses INFO-field specific for SnpEff ANN field, CADD field and remaining (other) fields.
+    """
+    for alt in range(0, len(alt_alleles)):
+        # Create INFO-dict for each alt allele
+        info_dict[alt_alleles[alt]] = {}
+        for info_line in info_field:
+            try:
+                # SnpEff ANN field
+                if info_line.split("=")[0] == "ANN":
+                    if vcf_metadata["INFO"]["ANN"]["description"] not in info_dict:
+                        ann_header = vcf_metadata["INFO"]["ANN"]["description"]
+                        ann_header = ann_header.split(" | ")
+                    ann_info = ""
+                    ann_info += info_line.split("=")[1]
+
+                    for ann_line in ann_info.split(","):
+                        alts = ann_line.split("|")
+                        annFieldDict(ann_info, ann_header, info_dict)
+
+                # CADD field
+                elif info_line.split("=")[0].startswith("CADD"):
+                    if "CADD" not in info_dict[alt_alleles[alt]].keys():
+                        info_dict[alt_alleles[alt]]["CADD"] = {}
+                    keys, values = createKeyVals(info_line, alt)
+
+                    info_dict[alt_alleles[alt]]["CADD"][keys.replace(".", "-")] = values
+                    # Convert datatype based on header (meta data) information
+                    info_dict[alt_alleles[alt]]["CADD"] = convert_data(info_dict[alt_alleles[alt]]["CADD"], vcf_metadata['INFO'])
+
+                # 'Other' field
+                else:
+                    keys, values = createKeyVals(info_line, alt)
+                    info_dict[alt_alleles[alt]][keys] = values
+            except IndexError:
+                # solve indexerror
+                pass
+
     for alt_index, alt_allele in enumerate(alt_alleles):
         pos, ref, alt = get_minimal_representation(fields[1], fields[3], alt_allele)
+
         variant = {
             'chr': fields[0],
             'pos' : pos,
             'ref' : ref,
             'alt' : alt,
+            'info' : info_dict[alt_allele]
         }
         variants.append(variant)
 
@@ -181,8 +260,8 @@ def gatk_line(line, vcf_metadata):
                         sample_var['filter'] = fields[6]
 
                     variant_samples.append(sample_var)
-
         variants_samples.append(variant_samples)
+
     return variants, variants_samples
 
 def delly_line(line, vcf_metadata):
@@ -248,16 +327,17 @@ def convert_data(data, metadata):
         if data[id] != ".":
             if ',' in data[id]:
                 data[id] = data[id].split(',')
-                if metadata[id]['type']  == "Integer":
-                    data[id] = map(int, data[id])
-                elif metadata[id]['type']  == "Float":
-                    data[id] = map(float, data[id])
 
-                else:
-                    if metadata[id]['type']  == "Integer":
-                        data[id] = int(data[id])
-                    elif metadata[id]['type']  == "Float":
-                        data[id] = float(data[id])
+            if type(data[id]) is list:
+                if metadata[id]['type']  == "Integer":
+                    data[id] = [int(item) if item != 'NA' else 'NA' for item in data[id]]
+                elif metadata[id]['type']  == "Float":
+                    data[id] = [float(item) if item != 'NA' else 'NA' for item in data[id]]
+            else:
+                if metadata[id]['type']  == "Integer":
+                    data[id] = int(data[id])
+                elif metadata[id]['type']  == "Float":
+                    data[id] = float(data[id])
     return data
 
 def adjust_genotype(gt_data, gt_format, alt_index, allele_symbol = '-'):
@@ -328,3 +408,37 @@ def get_minimal_representation(pos, ref, alt):
             ref = ref[1:]
             pos += 1
         return pos, ref, alt
+
+def createKeyVals(info_line, alt):
+    """
+    If a value consists of multiple transcripts that have identical values, only one will be stored.
+    Makes a keylist for all values before "=" & makes a valuelist for all values after "="
+    and returns these lists.
+    """
+    cadd_vals = info_line.split("=")[1]
+    if "," in cadd_vals:
+        multi_cadd = cadd_vals.split(",")
+        values = multi_cadd[alt].split("|")
+    else:
+        values = cadd_vals.split("|")
+    keys = info_line.split("=")[0]
+    return keys, values
+
+def annFieldDict(ann_info, ann_header, info_dict):
+    """
+    Create a ANN dictionary based on each transcript ID (FeatureID).
+    Will not store key/value pair if the value is empty.
+    """
+    # Replace all dots with an underscore (bulkwrite error solve)
+    ann_header = [i.replace(".", "_") for i in ann_header]
+    ann_transcript = [i.replace(".", "_") for i in ann_info.split(",")]
+
+    for ann_line in ann_transcript:
+        ann_values = ann_line.split("|")
+
+        # Create ANN-dict that parses each ANN-line.
+        # Use deep_update to update the info dictionary.
+        for line_pos in range(1, len(ann_header)):
+            if ann_values[line_pos]:
+                ann_dict = {ann_values[0]: {'ANN' : {ann_values[1]: {ann_values[6]: {ann_header[line_pos]: ann_values[line_pos]}}}}}
+                deep_update(info_dict, ann_dict)
